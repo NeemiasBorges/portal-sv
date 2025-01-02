c4f13ceb9ab3e869c26b8223ad64d266f92ca978
Neemias Borges <ifsp.neemiasb@gmail.com>
2025-01-02 12:14:02 -0300

    :wrench: chorus: Configuracao e estruturacao do projeto

--- backend-portal-sv/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ backend-portal-sv/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("backend-portal-sv")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96447af237e46109285c1a15cf132ee708829369")]
-[assembly: System.Reflection.AssemblyProductAttribute("backend-portal-sv")]
-[assembly: System.Reflection.AssemblyTitleAttribute("backend-portal-sv")]
+[assembly: System.Reflection.AssemblyProductAttribute("Domain")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
