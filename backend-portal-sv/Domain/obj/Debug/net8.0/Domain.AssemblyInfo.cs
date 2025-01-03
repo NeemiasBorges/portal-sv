ede88339d891b049f60e23eaad94d4aef1ce922e
Neemias Borges <ifsp.neemiasb@gmail.com>
2025-01-02 21:24:24 -0300

    ✨ feat: Ajustes dos arquivos para versionamentoi

--- backend-portal-sv/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ backend-portal-sv/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c04d7a7c97db14b322c5d8f3c286efd91031e66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edfb83d618dfe244bd8e6915556149118969bf05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ backend-portal-sv/backend-portal-sv/SwaggerConfig/ConfigureSwaggerGenOptions.cs
@@ -0,0 +1,44 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace backend_portal_sv.SwaggerConfig
+{
+    public class ConfigureSwaggerGenOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerGenOptions(IApiVersionDescriptionProvider provider)
+        {
+            _provider = provider ?? throw new ArgumentNullException(nameof(provider));
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateApiVersionInfo(description));
+            }
+        }
+
+        private OpenApiInfo CreateApiVersionInfo(ApiVersionDescription description)
+        {
+            return new OpenApiInfo
+            {
+                Title = "API de Seguro de Viagens",
+                Version = description.ApiVersion.ToString(),
+                Description = "API destinada ao portal de seguros de viagens, oferecendo integração com diversos serviços relacionados a seguros para viagens nacionais/internacionais.",
+                Contact = new OpenApiContact
+                {
+                    Name = "APISV",
+                    Email = "neemiasb.dev@gmail.com"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Licenca -> Teste Técnico - API"
+                }
+            };
+        }
+    }
+}
